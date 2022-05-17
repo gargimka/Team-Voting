596f8a50cb9b3224da2e711370b27e975739a399
Gargi <gargimka@gmail.com>
2022-05-17 13:12:04 +0530

    changed color

---
+++ Team-Voting-main/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC2t_D1pueO8pvjH6qgQb4nzR17m-JhF2M",
+  authDomain: "teamvotingapp-8fa51.firebaseapp.com",
+  projectId: "teamvotingapp-8fa51",
+  storageBucket: "teamvotingapp-8fa51.appspot.com",
+  messagingSenderId: "129436903152",
+  appId: "1:129436903152:web:6ee4e252958d459422c435"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
